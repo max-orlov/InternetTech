d8a6501093019c445047cc905eee7744673a7184
Tom <tomhoffen@gmail.com>
2014-12-21 18:38:37 +0200

    update

--- 03/handlers/requestHandlers.js
+++ 03/handlers/requestHandlers.js
@@ -15,23 +15,13 @@ function start(request, rootFolder, parser, socket) {
     console.log("Request handler 'start' was called.");
 
     var response = new Response();
+    var fileType = request.path.substr(request.path.lastIndexOf('.') + 1);
+
     response.httpVersion =  serverSettings.HTTP_SUPPORTED_VERSIONS['1.1'];
-
-
-    //TODO:: I think we should support html v1.0
-    if (request.httpVersion == serverSettings.HTTP_SUPPORTED_VERSIONS['1.1'])
-    {
-        response.status = serverSettings.STATUS_CODES['200'];
-        response.headers['Date'] = new(Date)().toUTCString();
-        var fileType = request.path.substr(request.path.lastIndexOf('.') + 1);
-        response.headers['Content-Type'] = serverSettings.CONTENT_TYPES[fileType];
-        response.headers['Server'] = serverSettings.SERVER_VERSION;
-
-    }
-    else{
-        response.status = serverSettings.STATUS_CODES['505'];
-
-    }
+    response.status = serverSettings.STATUS_CODES['200'];
+    response.headers['date'] = new(Date)().toUTCString();
+    response.headers['content-type'] = serverSettings.CONTENT_TYPES[fileType];
+    response.headers['server'] = serverSettings.SERVER_VERSION;
 
     if(request != null && request.status === "Done") {
         var normPath = path.normalize(rootFolder + request.path);
@@ -45,9 +35,8 @@ function start(request, rootFolder, parser, socket) {
             }
         });
     }
-
     return true;
-};
+}
 
 function writeHeaders(response, parser, socket) {
     socket.write(parser.stringify(response));
