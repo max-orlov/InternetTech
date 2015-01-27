669ce56bab6d15b82537203c20b7c0d26f4ee2e1
Tom <tomhoffen@gmail.com>
2015-01-27 22:43:28 +0200

    Status file change

--- 05/handlers/requestBodyHandler.js
+++ 05/handlers/requestBodyHandler.js
@@ -1,10 +1,9 @@
 var RequestJsonHandler          = require('./requestJsonHandler'),
     RequestUrlEncodedHandler    = require('./requestUrlEncodedHandler'),
-    serverSettings              = require('./../settings/settings'),
     mimeTypes                   = require('./../settings/mimeTypes');
 function RequestBodyHandler() {
     return function (request, response, next) {
-        if(request.is(serverSettings.serverVersion['json'])) {
+        if (request.is(mimeTypes.getMimeType('json'))) {
             return RequestJsonHandler()(request, response, next);
         } else if (request.is(mimeTypes.getMimeType('xform'))) {
             return RequestUrlEncodedHandler()(request, response, next);
